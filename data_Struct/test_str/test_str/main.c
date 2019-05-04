40fe5fe0dd89f7122cf5a6ebc004cdca5af3345d
JinHyoukYork <jdd04026@gmail.com>
2019-05-04 13:17:29 +0900

    TIL_clean_DIR

--- data_Struct/test_str/test_str/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(void){
-    int value1=-1, value2=-1;
-    char a=0xf0;
-    signed char b=0xf0;
-    unsigned char c = 0xf0;
-    printf("%d %d", b, c);
-    return 0;
-}
