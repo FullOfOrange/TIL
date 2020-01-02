a5ca1dda3cffcb3df3ab9671c8dbc3fb7f8b65c5
FullOfOrange <foorgdev@gmail.com>
2020-01-02 14:26:35 +0900

    위장 문제 주석 업데이트

--- Algorithm/Hash/위장.java
+++ Algorithm/Hash/위장.java
@@ -7,15 +7,16 @@ class Solution {
         int answer = 1;
         // 아래의 부분은 각각 의상의 개수를 세기 위해서 해쉬맵 사용
         Map<String, Integer> map = new HashMap<>();
-        for(int i = 0; i < clothes.length; i++){
+        for (int i = 0; i < clothes.length; i++) {
             Integer temp = map.get(clothes[i][1]);
-            if(temp == null) temp = 0;
-            map.put(clothes[i][1],temp+1);       
+            if (temp == null)
+                temp = 0;
+            map.put(clothes[i][1], temp + 1);
         }
-        // 1.      
+        // 1.
         // (a+1) * (b+1) * ... - 1 의 공식인데 이것을 만족하는 알고리즘만 짜면 된다...
         // 이것을 간단히 풀어보자.
-        // ab + a + b 인데, 이렇게 하면 각각의 모든 경우의 수를 뽑을 수 있다.
+        // ab + a + b 인데, 이렇게 하면 각각의 모든 경우의 수를 뽑을 수 있다. 머리 개좋다.
         // 2.
         // 순열 nPr - n개의 카드 중에서 r개를 뽑아서 순서 있게 나열할 수 있는 경우의 수.
         // 조합 nCr - n개의 카드 중에서 r개를 뽑아서 순서 없이 무작위로 나열할 수 있는 경우의 수.
@@ -23,9 +24,9 @@ class Solution {
         // 모든 경우의 수를 구하면 된다.
 
         Iterator<Integer> it = map.values().iterator();
-        while(it.hasNext()) {
-            answer *= it.next().intValue()+1;
+        while (it.hasNext()) {
+            answer *= it.next().intValue() + 1;
         }
-        return answer-1;
+        return answer - 1;
     }
 }
